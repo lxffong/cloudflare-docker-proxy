50e7d13408d40d7ce338967826afdc8753ec730c
lxffong <lxffong@gmail.com>
2024-06-26 10:49:31 +0800

    update domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.fninit.com": dockerHub,
+  "quay.fninit.com": "https://quay.io",
+  "gcr.fninit.com": "https://gcr.io",
+  "k8s-gcr.fninit.com": "https://k8s.gcr.io",
+  "k8s.fninit.com": "https://registry.k8s.io",
+  "ghcr.fninit.com": "https://ghcr.io",
+  "cloudsmith.fninit.com": "https://docker.cloudsmith.io",
+  "ecr.fninit.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.fninit.com": dockerHub,
 };
 
 function routeByHosts(host) {
